db9c224791ae1e8e516bce2b4c96342fca473250
peipei1234 <1564562711@qq.com>
2019-11-12 20:33:06 +0800

    黑马头条PC-初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import element from 'element-ui'
+import elementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-Vue.use(element)
+Vue.use(elementUi)
 new Vue({
   render: h => h(App),
   router
