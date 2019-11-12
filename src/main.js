5d6bdd594b030df31a09d810a3f62393201dcc09
peipei1234 <1564562711@qq.com>
2019-11-12 16:34:32 +0800

    引入element-ui

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-
+Vue.use(element)
 new Vue({
-  router,
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
